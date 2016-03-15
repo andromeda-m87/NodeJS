8622e18bb4e53ea985f92d20a56c9c86bb86847d
prashanth081188 <prashanth.manohar8@gmail.com>
2016-03-15 10:40:08 -0500

    Added files via upload

---
+++ config/database.js
@@ -0,0 +1,8 @@
+// config/database.js
+module.exports = {
+
+    //'url' : ''postgres://dbuser:dbpasswd@dbhost:5432/dbname'
+    //'url' : 'postgres://postgres@localhost:5432/employees'
+	'url': 'postgres://fsxcutbajxjqep:ytQWiQsw-KC2ABslgwQOvo_EF7@ec2-54-225-151-64.compute-1.amazonaws.com:5432/d7r6ih525dt9rt'
+
+};
